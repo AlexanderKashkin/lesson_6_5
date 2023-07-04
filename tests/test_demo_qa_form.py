4a03140e902b1826e09b4343cb780ca9bceb429f
Alexander <a.kashkin@sigma.ru>
2023-07-04 15:58:48 +0300

    test form mid-level-step-obj

--- tests/test_demo_qa_form.py
+++ tests/test_demo_qa_form.py
@@ -2,63 +2,39 @@ import os
 
 from config import browser as b
 from selene import be, have
+from lesson_6_5.demoqa_form import StudentRegistrationForm
 
 
 def test_form(set_window_size):
-    first_name = 'Alexey'
-    last_name = 'Ivanov'
-    email = f'{first_name.lower()}_{last_name.lower()}@email.com'
-    gender = 'Male'
-    year_birthday = '1990'
-    month_birthday = 'February'
-    day_birthday = '006'
-    subject = 'Maths'
-    hobby = 'Sports'
-    path_for_picture = 'image/meme.jpg'
-    phone = '1234567890'
-    address = 'my_address'
-    state = 'NCR'
-    city = 'Noida'
-
-    b.open('automation-practice-form')
-    b.should(have.title('DEMOQA'))
-    b.element('[class="main-header"]').should(have.text('Practice Form'))
-
-    b.element('#firstName').should(be.blank).click().type(first_name)
-    b.element('#lastName').should(be.blank).click().type(last_name)
-    b.element('#userEmail').should(be.blank).click().type(email)
-    b.element('[for="gender-radio-1"]').click()
-    b.element('#userNumber').should(be.blank).click().type(phone)
-
-    b.element('#dateOfBirthInput').click()
-    b.element('[class="react-datepicker__year-select"]').click().type(year_birthday).click()
-    b.element('[class="react-datepicker__month-select"]').click().type(month_birthday).click()
-    b.element(f'[class="react-datepicker__day react-datepicker__day--{day_birthday}"]').click()
-
-    b.element('#subjectsInput').type(subject).press_enter()
-    b.element(f'//label[contains(text(), "Sports")]').click()
-    b.element('#currentAddress').should(be.blank).click().type(address)
-
-    b.element('#uploadPicture').send_keys(os.path.abspath(path_for_picture))
-
-    b.element('#state').click()
-    b.element('#react-select-3-input').type(state).press_enter()
-    b.element('#react-select-4-input').type(city).press_enter()
-    b.element('#submit').click()
+    reg_form = StudentRegistrationForm()
+    reg_form.open()
+    reg_form.fill_first_name()
+    reg_form.fill_last_name()
+    reg_form.fill_email()
+    reg_form.choose_gender()
+    reg_form.fill_mobile()
+    reg_form.fill_data_of_birth()
+    reg_form.fill_subj()
+    reg_form.choose_hobbies()
+    reg_form.select_picture()
+    reg_form.fill_current_address()
+    reg_form.fill_state()
+    reg_form.fill_city()
+    reg_form.close()
 
     b.element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
     b.all('.table-responsive>table>tbody>tr').should(have.size(10))
-
-    b.all('.table-responsive tr').element_by(have.text('Student Name')).should(have.text(f'{first_name} {last_name}'))
-    b.all('.table-responsive tr').element_by(have.text('Student Email')).should(have.text(f'{email}'))
-    b.all('.table-responsive tr').element_by(have.text('Gender')).should(have.text(f'{gender}'))
-    b.all('.table-responsive tr').element_by(have.text('Mobile')).should(have.text(f'{phone}'))
-    b.all('.table-responsive tr').element_by(have.text('Date of Birth')).should(
-        have.text(f'{day_birthday[1:]} {month_birthday},{year_birthday}'))
-    b.all('.table-responsive tr').element_by(have.text('Subjects')).should(have.text(f'{subject}'))
-    b.all('.table-responsive tr').element_by(have.text('Hobbies')).should(have.text(f'{hobby}'))
-    b.all('.table-responsive tr').element_by(have.text('Picture')).should(have.text('meme.jpg'))
-    b.all('.table-responsive tr').element_by(have.text('Address')).should(have.text(f'{address}'))
-    b.all('.table-responsive tr').element_by(have.text('State and City')).should(have.text(f'{state} {city}'))
-
-    b.element('[id="closeLargeModal"]').click()
+    reg_form.table_responsive.element_by(have.text('Student Name')).should(
+        have.text(f'{reg_form.first_name} {reg_form.last_name}'))
+    reg_form.table_responsive.element_by(have.text('Student Email')).should(have.text(f'{reg_form.email}'))
+    reg_form.table_responsive.element_by(have.text('Gender')).should(have.text(f'{reg_form.gender}'))
+    reg_form.table_responsive.element_by(have.text('Mobile')).should(have.text(f'{reg_form.phone}'))
+    reg_form.table_responsive.element_by(have.text('Date of Birth')).should(
+        have.text(f'{reg_form.day_birthday[1:]} {reg_form.month_birthday},{reg_form.year_birthday}'))
+    reg_form.table_responsive.element_by(have.text('Subjects')).should(have.text(f'{reg_form.subject}'))
+    reg_form.table_responsive.element_by(have.text('Hobbies')).should(have.text(f'{reg_form.hobby}'))
+    reg_form.table_responsive.element_by(have.text('Picture')).should(have.text('meme.jpg'))
+    reg_form.table_responsive.element_by(have.text('Address')).should(have.text(f'{reg_form.address}'))
+    reg_form.table_responsive.element_by(have.text('State and City')).should(
+        have.text(f'{reg_form.state} {reg_form.city}'))
+    reg_form.close_modal_window()
